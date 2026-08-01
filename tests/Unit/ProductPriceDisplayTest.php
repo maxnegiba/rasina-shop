29484153960fcf8de9eb3b383034863d05eb6119
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-01 14:37:35 +0300

    Test unique product purchase eligibility

--- tests/Unit/ProductPriceDisplayTest.php
+++ tests/Unit/ProductPriceDisplayTest.php
@@ -26,4 +26,37 @@ class ProductPriceDisplayTest extends TestCase
 
         $this->assertSame('Preț la cerere', $product->displayPrice());
     }
+    public function test_a_unique_product_uses_the_normal_purchase_flow_when_priced_and_in_stock(): void
+    {
+        $product = new Product([
+            'is_custom' => true,
+            'price' => 1250,
+            'stock' => 1,
+        ]);
+
+        $this->assertTrue($product->isPurchasable());
+    }
+
+    public function test_a_unique_product_without_a_valid_price_cannot_enter_checkout(): void
+    {
+        $product = new Product([
+            'is_custom' => true,
+            'price' => null,
+            'stock' => 1,
+        ]);
+
+        $this->assertFalse($product->isPurchasable());
+    }
+
+    public function test_a_unique_product_without_stock_cannot_enter_checkout(): void
+    {
+        $product = new Product([
+            'is_custom' => true,
+            'price' => 1250,
+            'stock' => 0,
+        ]);
+
+        $this->assertFalse($product->isPurchasable());
+    }
+
 }
