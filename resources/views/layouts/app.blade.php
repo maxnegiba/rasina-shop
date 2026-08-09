027c36b27d80bd843853e98256788fdf5a5e3816
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-09 17:33:57 +0300

    Fix deferred Manrope URL serialization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
 
                     const link = document.createElement('link');
                     link.rel = 'stylesheet';
-                    link.href = '{{ $homeManropeStylesheet }}';
+                    link.href = 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600&display=optional';
                     link.media = 'print';
                     link.dataset.homeManrope = 'true';
                     link.onload = function () {
