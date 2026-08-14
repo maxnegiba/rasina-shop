7f84fc320fa185bfef49999590368541d04d23a3
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-14 15:56:51 +0300

    add editable contact and about page settings

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -15,6 +15,37 @@ class GeneralSettings extends Settings
     public string $default_whatsapp_greeting_text;
     public array $working_hours;
 
+    public string $footer_description;
+
+    public string $contact_eyebrow;
+    public string $contact_title;
+    public string $contact_intro;
+    public string $contact_address_label;
+    public string $contact_address_note;
+    public string $contact_communication_label;
+    public string $contact_hours_label;
+    public string $contact_custom_card_title;
+    public string $contact_custom_card_text;
+    public string $contact_custom_card_cta;
+    public string $contact_form_title;
+    public string $contact_form_response_note;
+    public string $contact_custom_eyebrow;
+    public string $contact_custom_title;
+    public string $contact_custom_intro;
+
+    public string $about_eyebrow;
+    public string $about_title;
+    public string $about_image;
+    public string $about_image_alt;
+    public string $about_quote;
+    public string $about_paragraph_one;
+    public string $about_paragraph_two;
+    public string $about_value_one_title;
+    public string $about_value_one_text;
+    public string $about_value_two_title;
+    public string $about_value_two_text;
+    public string $about_cta_label;
+
     public static function group(): string
     {
         return 'general';
