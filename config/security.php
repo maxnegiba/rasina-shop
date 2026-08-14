efc3e17400325aaa3ddc290e3201245a6afe9b98
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-14 13:39:23 +0300

    Remove duplicate MFA session timers

--- config/security.php
+++ config/security.php
@@ -3,14 +3,6 @@
 return [
     'admin_mfa' => [
         'code_seconds' => (int) env('ADMIN_MFA_CODE_SECONDS', 600),
-
-        'idle_seconds' => (int) env(
-            'ADMIN_MFA_IDLE_SECONDS',
-            env('ADMIN_MFA_SESSION_SECONDS', 7200),
-        ),
-
-        'absolute_seconds' => (int) env('ADMIN_MFA_ABSOLUTE_SECONDS', 43200),
-
         'send_attempts' => (int) env('ADMIN_MFA_SEND_ATTEMPTS', 3),
         'verify_attempts' => (int) env('ADMIN_MFA_VERIFY_ATTEMPTS', 5),
     ],
