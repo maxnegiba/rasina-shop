312d32a6e22b743d35ae56edc971de73f5368d15
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-20 17:37:18 +0300

    feat(marketing): configure Meta Conversions API

--- config/marketing.php
+++ config/marketing.php
@@ -6,4 +6,10 @@ return [
     'gtm' => [
         'container_id' => env('GTM_CONTAINER_ID'),
     ],
+
+    'meta' => [
+        'pixel_id' => env('META_PIXEL_ID'),
+        'capi_access_token' => env('META_CAPI_ACCESS_TOKEN'),
+        'graph_api_version' => env('META_GRAPH_API_VERSION', 'v23.0'),
+    ],
 ];
