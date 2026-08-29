a47da7f9e9396b4e9916f0e01cdc34de3fb2a9bf
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-29 11:21:21 +0300

    Add Pinterest domain verification meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="p:domain_verify" content="7f3b36eeb44634f2c91b67046d99d844">
     @hasSection('seo_meta')
         @yield('seo_meta')
     @else
